8e43b185b58ead082a56f6fbf6baa8a3286dcb59
Smit Pawar <dynaster21@gmail.com>
2025-08-01 01:23:07 +0530

    change in format

--- src/store/useQuizStore.js
+++ src/store/useQuizStore.js
@@ -1,7 +1,19 @@
 import { create } from "zustand";
 
 export const useQuizStore = create((set, get) => ({
-    quizData: [],
+    quizData: [
+        {
+            id: 1,
+            question: "",
+            options: [
+                "A",
+                "B",
+                "C",
+                "D"
+            ],
+            correctAnswer: 1
+        }
+    ],
 
     setQuizData: (data) => {
         set({ quizData: data })
